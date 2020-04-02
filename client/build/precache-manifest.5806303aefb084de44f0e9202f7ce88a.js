b27597b70594eaf86efe954968f41932941dab7b
CameronLumpkinATL <camlump@yahoo.com>
2020-04-01 21:49:16 -0400

    initial commit

---
+++ client/build/precache-manifest.5806303aefb084de44f0e9202f7ce88a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089aa4c9db547c4bffab4a78617bcfa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc4a7ecdb07286e9bfae",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "dc4a7ecdb07286e9bfae",
+    "url": "/static/js/main.94b11807.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
